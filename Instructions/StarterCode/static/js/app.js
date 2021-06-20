a153c8dc2b0c4d5995a4fcff984e2afa3782cc15
Cheyenne M <76500260+martiche93@users.noreply.github.com>
2021-06-20 14:22:09 -0700

    Update of app.py code

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -19,3 +19,32 @@ Object.entries(sightings).forEach(([key, value]) => {
     cell.text(value)
     });
 });
+
+// Create a step to psuh the 'Filter Table' button
+// Use Developer Tool to see where the 'Filter Table' button exists. Use d3 to select it
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+    tbody.html('');
+    // Select the 'Enter a Date'
+    var inputElement = d3.select("#datetime");
+    // Get the value property of the input date, state, shape
+    var inputValue = inputElement.property("value");
+    // Print input value
+    console.log(inputValue);
+    // Filter data with datetime equal to inputValue
+    var filterData = tableData.filter(sightings => sightings.datetime === inputValue);
+    // Print filterData
+    console.log(filterData);
+
+    filterData.forEach(selections => {
+        console.log(selections);
+        // Append one table row for each sighting object
+        var row = tbody.append("tr");
+        // Use Object.entries to print each sighting value
+        Object.entries(selections).forEach(([key, value]) => {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
