51e13a13d3e5899f0c6321f04dbe8c2ea826463c
Cheyenne M <76500260+martiche93@users.noreply.github.com>
2021-06-20 16:32:20 -0700

    removed DS file from mainpage

--- UFO-level-2/StarterCode/static/js/app.js
+++ UFO-level-2/StarterCode/static/js/app.js
@@ -4,15 +4,18 @@ var tblColumns = ["datetime", "city", "state", "country", "shape", "durationMinu
 
 // Use d3 to select the table body. Set table body as variable
 var tbody = d3.select("tbody");
+// Use d3 to select the Search and Reset button. Set both as a variable
 var btnSearch = d3.select("#btnSearch");
 var btnReset = d3.select("#btnReset");
 
+// Use d3 to select each category needed for Level 2
 var searchDate = d3.select("#searchDate");
 var searchCity = d3.select("#searchCity");
 var searchState = d3.select("#searchState");
 var searchCountry = d3.select("#searchCountry");
 var searchShape = d3.select("#searchShape");
 
+// Load table rows into table body
 var loadTableRows = (whichData) => {
     tbody.html("");
 
@@ -25,6 +28,7 @@ var loadTableRows = (whichData) => {
 
 loadTableRows(tableData);
 
+// Set up Search button for the listed properties below
 btnSearch.on("click", () => {
     d3.event.preventDefault();
 
@@ -33,12 +37,16 @@ var searchedCity = searchCity.property("value");
 var searchedState = searchState.property("value");
 var searchedCountry = searchCountry.property("value");
 var searchedShape = searchShape.property("value");
-    
+
+// Set up if/else/else if statements for each item that doesn't return a search
     if (searchedDate) {
+        // Filter the data to only determine the searchedDate value
         var tableData_Filtered = tableData.filter(tableData => tableData.datetime === searchedDate);
+        // When filtered data does not equal zero, add to table rows
         if (tableData_Filtered.length !== 0) {
             loadTableRows(tableData_Filtered);
         }
+        // If filtered data does equal zero, print message that the search wasn't found
         else {
             tbody.html("");
             tbody.append("tr").append("td").text("No such sightings were found. Please try again.");
@@ -85,6 +93,7 @@ var searchedShape = searchShape.property("value");
         }
     };
 
+// Set up Reset Button with "click", set to each search value
 btnReset.on("click", () => {
 	document.getElementById("searchDate").value='';
 	document.getElementById("searchCity").value='';
@@ -92,7 +101,7 @@ btnReset.on("click", () => {
 	document.getElementById("searchCountry").value='';
 	document.getElementById("searchShape").value='';
 	
-	// Load original dataset
+// Load original dataset into table
 	loadTableRows(tableData);
     });
 });
